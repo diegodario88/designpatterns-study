19c1c59911dd58157ee718317a06fb6f9f47acee
Diego Dario <diegodario88@gmail.com>
2021-06-13 21:51:23 -0300

    add Observer

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,89 @@
-import { BaseRecord, Database, Pokemon } from "./types/shared";
+import {
+    BaseRecord,
+    Database,
+    Pokemon,
+    ObserverPayload,
+    Listener,
+    AfterSetEvent,
+    BeforeSetEvent
+} from "./types/shared";
+
+/**@description Observer (Pub/Sub) example */
+function createObserver<EventType>(): ObserverPayload<EventType> {
+    let listeners: Listener<EventType>[] = [];
+
+    return {
+        subscribe: (listener: Listener<EventType>) => {
+            listeners.push(listener);
+            return () => {
+                listeners = listeners.filter((l) => l !== listener)
+            }
+        },
+        publish: (event: EventType) => {
+            listeners.forEach((l) => l(event));
+        }
+    }
+}
 
 /**@description Factory example returning a Singleton */
 function factoryDatabase<T extends BaseRecord>() {
     class InMemoryDatabase implements Database<T> {
-        private constructor(){}
+        private constructor() { }
         private records: Record<string, T> = {};
+        private beforeAddListeners = createObserver<BeforeSetEvent<T>>();
+        private afterAddListeners = createObserver<AfterSetEvent<T>>();
+
         static client: InMemoryDatabase = new InMemoryDatabase();
 
         public set(newValue: T): this {
             const key = newValue.id.toLowerCase();
+
+            this.beforeAddListeners.publish({
+                newIncomeValue: newValue, value: this.records[key]
+            });
+
             this.records[key] = newValue;
+
+            this.afterAddListeners.publish({
+                value: this.records[key]
+            });
+
             return this;
         }
-    
+
         public get(id: string): T | undefined {
             const key = id.toLowerCase();
             return this.records[key];
+        }
+
+        onBeforeAdd(listener: Listener<BeforeSetEvent<T>>) {
+            return this.beforeAddListeners.subscribe(listener);
+        }
+
+        onAfterAdd(listener: Listener<AfterSetEvent<T>>) {
+            return this.afterAddListeners.subscribe(listener);
         }
     }
 
     return InMemoryDatabase;
 }
 
-
 const DB = factoryDatabase<Pokemon>();
 
-const result = DB.client.set({id: 'Bulbasaur', attack: 50, defense: 20});
+const unsubscribe = DB.client.onAfterAdd(({ value }) => console.log(value));
 
-console.log(result);
+DB.client
+    .set({ id: 'Bulbasaur', attack: 50, defense: 20 })
+    .set({ id: 'Charmander', attack: 40, defense: 30 })
+
+unsubscribe();
+
+DB.client.set({ id: 'Venusaur', attack: 30, defense: 50 });
+DB.client.set({ id: 'Ivysaur', attack: 40, defense: 40 });
+
+DB.client.onBeforeAdd(
+    ({ newIncomeValue, value }) => console.table([newIncomeValue, value])
+);
+
+DB.client.set({ id: 'Venusaur', attack: 35, defense: 60 });
+
--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,3 +1,4 @@
+export type Listener<T> = (env: T) => void; 
 export interface Pokemon {
     id: string;
     attack: number;
@@ -11,4 +12,20 @@ export interface BaseRecord {
 export interface Database<T extends BaseRecord> {
     set(newValue: T): void;
     get(id: string): T | undefined;
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
+}
+
+export interface ObserverPayload<T> {
+    subscribe: (listener: Listener<T>) => () => void;
+    publish: (event: T) => void; 
+}
+
+export interface BeforeSetEvent<T> {
+    value: T;
+    newIncomeValue: T;
+}
+
+export interface AfterSetEvent<T> {
+    value: T;
 }
